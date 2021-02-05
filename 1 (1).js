f3b3143347f16b65546d8e6c3a11432279612919
regycloud <47989041+regycloud@users.noreply.github.com>
2021-02-05 20:43:23 +0700

    Delete 1 (1).js

--- 1 (1).js
+++
@@ -1,9 +0,0 @@
-/*
-Budi membuka sebuah toko vape di sebelah rumahnya.
-Calon pelanggan yang boleh datang ke tokonya harus berumur 18++, membawa KTP, memiliki kartu membership.
-Karena saat ini sedang Corona, Budi juga harus memeriksa suhu tubuh calon pelanggannya.
-Jika suhu tubuhnya di atas 37c maka calon pelanggan tidak boleh masuk.
-Buatlah sebuah program yang dapat membantu Budi dalam memeriksa calon pelanggan yang masuk.
-Jika dia tidak boleh masuk, maka tampilkan, "Calon pelanggan ini tidak boleh masuk".
-Jika boleh, maka tampilkan "Calon pelanggan boleh masuk".
-*/
